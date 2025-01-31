d88e89f38dba5a8a8aed9468426759db120107a3
ponurakk <p0unrakg@int.pl>
2025-01-31 23:09:56 +0100

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ymir
+
+TUI successor of unfinished [https://github.com/ponurakk/Project-Manager]
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use ratatui::crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> anyhow::Result<()> {
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> anyhow::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
